302e7e319e6f4f1b07ce055fbba26c0470335d16
Kali9kov <119761984+Kali9kov@users.noreply.github.com>
2023-04-03 16:01:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,39 +21,42 @@ document.querySelectorAll('nav a').forEach(link => {
   
       // Reset terminal height when switching sections
       const terminalBody = document.querySelector('.terminal-body');
-      terminalBody.style.height = '300px';
+      terminalBody.style.height = defaultHeight;
       terminalBody.style.overflowY = 'hidden';
     });
   });
   
+  const defaultHeight = '300px';
+
   document.querySelector('nav a').click(); // Trigger the initial section display
   
   // Green button functionality
   document.querySelector('.terminal-button.green').addEventListener('click', () => {
     const terminalBody = document.querySelector('.terminal-body');
-    if (terminalBody.style.height === '300px') {
+    if (terminalBody.style.height === defaultHeight) {
       terminalBody.style.height = '400px';
       if (terminalBody.scrollHeight > terminalBody.clientHeight) {
         terminalBody.style.overflowY = 'scroll';
       }
     } else {
-      terminalBody.style.height = '300px';
+      terminalBody.style.height = defaultHeight;
       terminalBody.style.overflowY = 'hidden';
     }
   });
 
   // Red button functionality
-document.querySelector('.terminal-button.red').addEventListener('click', () => {
+  document.querySelector('.terminal-button.red').addEventListener('click', () => {
     const homeLink = document.querySelector('nav a[href="#home"]');
     homeLink.click();
   });
   
-  // Orange button functionality
   document.querySelector('.terminal-button.yellow').addEventListener('click', () => {
     const terminalBody = document.querySelector('.terminal-body');
-    if (terminalBody.style.display !== 'none') {
-      terminalBody.style.display = 'none';
+    if (terminalBody.style.height === defaultHeight) {
+    terminalBody.style.height = '0';
     } else {
-      terminalBody.style.display = 'block';
+    terminalBody.style.height = defaultHeight;
     }
   });
+
+  const terminalSections = document.querySelectorAll('.terminal-content');
